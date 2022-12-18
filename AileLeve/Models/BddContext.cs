d88720c2e0d1019d73e81b78cfff12b513f26856
Melanie Rulof <bancelin.melanie@gmail.com>
2022-12-18 19:03:14 +0100

    ajout des images de profil

--- AileLeve/Models/BddContext.cs
+++ AileLeve/Models/BddContext.cs
@@ -107,17 +107,17 @@ namespace AileLeve.Models
             ); ;
 
             this.Profils.AddRange(
-                new Profil { Id = 1, Telephone = "0625522552", Image = "/img/profil.jpg", Email = "dupondjean@gmail.fr" },
-                new Profil { Id = 2, Telephone = "0725752552", Image = "/img/profil.jpg", Email = "dupontyann@yahoo.fr" },
-                new Profil { Id = 3, Telephone = "0635528550", Image = "/img/profil.jpg", Email = "dylanbob@gmail.fr" },
-                new Profil { Id = 4, Telephone = "0600548552", Image = "/img/profil.jpg", Email = "bibbjustine@hotmail.fr" },
-                new Profil { Id = 5, Telephone = "0607788908", Image = "/img/profil.jpg", Email = "pierre.durand@gmail.com" },
-                new Profil { Id = 6, Telephone = "0607788908", Image = "/img/profil.jpg", Email = "robert.frost@gmail.com" },
-                new Profil { Id = 7, Telephone = "0607788908", Image = "/img/profil.jpg", Email = "utilisateur.aileleve@gmail.com" },
+                new Profil { Id = 1, Telephone = "0625522552", Image = "/img/DupondJean.jpg", Email = "dupondjean@gmail.fr" },
+                new Profil { Id = 2, Telephone = "0725752552", Image = "/img/DupontYann.png", Email = "dupontyann@yahoo.fr" },
+                new Profil { Id = 3, Telephone = "0635528550", Image = "/img/DylanBob.jpg", Email = "dylanbob@gmail.fr" },
+                new Profil { Id = 4, Telephone = "0600548552", Image = "/img/BibbJustine.jpg", Email = "bibbjustine@hotmail.fr" },
+                new Profil { Id = 5, Telephone = "0607788908", Image = "/img/DurandPierre.jpg", Email = "pierre.durand@gmail.com" },
+                new Profil { Id = 6, Telephone = "0607788908", Image = "/img/FrostRobert.png", Email = "robert.frost@gmail.com" },
+                new Profil { Id = 7, Telephone = "0607788908", Image = "/img/RuskinJohn.jpg", Email = "utilisateur.aileleve@gmail.com" },
                 new Profil { Id = 8, Telephone = "0607788908", Image = "/img/profil.jpg", Email = "christian.bobin@gmail.com" },
-                new Profil { Id = 9, Telephone = "0607788908", Image = "/img/profil.jpg", Email = "marcel.proust@gmail.com" },
-                new Profil { Id = 10, Telephone = "0607788908", Image = "/img/profil.jpg", Email = "jeanmichel.maulpoix@gmail.com" },
-                new Profil { Id = 11, Telephone = "0102030405", Image = "/img/profil.jpg", Email = "aileleve.soutienscolaire@gmail.com" }
+                new Profil { Id = 9, Telephone = "0607788908", Image = "/img/ProustMarcel.jpg", Email = "marcel.proust@gmail.com" },
+                new Profil { Id = 10, Telephone = "0607788908", Image = "/img/MaulpoixJM.jpg", Email = "jeanmichel.maulpoix@gmail.com" },
+                new Profil { Id = 11, Telephone = "0102030405", Image = "/img/Aile'lèveV3.png", Email = "aileleve.soutienscolaire@gmail.com" }
             );
 
             this.Comptes.AddRange(
